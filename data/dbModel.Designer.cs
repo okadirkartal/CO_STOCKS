84eef8bf95bdf2bbb48cd0c22d98c64bf0fe5935
okadirkartal <okadirkartal@gmail.com>
2016-09-22 15:22:40 +0300

    last fixes

--- data/dbModel.Designer.cs
+++ data/dbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\kkartal\Documents\Visual Studio 2015\Projects\CO_Stock\data\dbModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\volna81\Source\Repos\AppStock\data\dbModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
